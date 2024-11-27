f36097637d266b2fbf4b91be4d59081735471e35
123456789067g <121591771+123456789067g@users.noreply.github.com>
2024-11-27 14:47:15 -0800

    Update CrossingFreePartition.py

--- src/CrossingFreePartition.py
+++ src/CrossingFreePartition.py
@@ -23,13 +23,23 @@ class CrossingFreePartition:
             for convexhull in self.convexhulls:
                 if new_convexhull.is_intersecting(convexhull) == True:
                     return
-
+            self.convexhulls.append(new_convexhull)
 
         self.is_valid = True
 
 
     def is_distance_1_from(self, crossing_partition):
-        
-        return
+        for convexhull1 in self.convexhulls:
+            for convexhull2 in crossing_partition.convexhulls:
+                if self.are_convexhulls_distance_1(convexhull1, convexhull2):
+                    return True
+        return False
             
+    def are_convexhulls_distance_1(self, convexhull1, convexhull2):
+        for point1 in convexhull1.points:
+            for point2 in convexhull2.points:
+                distance = np.linalg.norm(point1 - point2)  # 计算两点之间的欧几里得距离
+                if distance == 1:
+                    return True
+        return False
             
